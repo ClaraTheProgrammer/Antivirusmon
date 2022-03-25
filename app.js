79df4252eb39f0913e2a37695714b2838e70103a
jadenUMBC <crexte123@hotmail.com>
2022-03-24 20:48:45 -0400

    add homePage and resultsPage

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 
+//GET request to root of web page and render homePage.html file
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.sendFile(__dirname + "/homePage.html");
 })
+
+
+//GET request to /results and render resultsPage.html file
+app.get('/results', function (req, res) {
+    res.sendFile(__dirname + "/resultsPage.html");
+  })
 
 app.listen(3000)
