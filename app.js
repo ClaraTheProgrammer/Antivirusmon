3f3d25e1ee51be9bd4eae03e62e4fd446a3d1633
jadenUMBC <crexte123@hotmail.com>
2022-03-31 12:40:42 -0400

    set up sql, insert hashes into sql

--- app.js
+++ app.js
@@ -1,5 +1,58 @@
 const express = require('express')
 const app = express()
+var mysql = require('mysql');
+const readline = require('readline');
+const fs = require('fs');
+
+//database server connection information
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root"
+});
+
+//connect to database server
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+  //create database within database server
+  con.query("CREATE DATABASE maliciousHashDB", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+
+  //connect to database 'mydb'
+  con.query("USE maliciousHashDB", function (err, result) {
+    if (err) throw err;
+    console.log("connected to 'maliciousHashDB' database");
+  });
+
+  //create table
+  var createTableQuery = "CREATE TABLE maliciousHash (hashID VARCHAR(16000))";
+  con.query(createTableQuery, function (err, result) {
+    if (err) throw err;
+    console.log("created table 'maliciousHash'");
+  });
+
+
+  //set up read file interface
+  var readLine = require('readline').createInterface({
+    input: require('fs').createReadStream('maliciousHashes.txt')
+  });
+
+  //read maliciousHashes.txt file line by line and insert hash into database
+  readLine.on('line', function (line) {
+    var insertQuery = `INSERT INTO maliciousHash (hashID) VALUES ('${line}')`;
+    con.query(insertQuery, function (err, result) {
+      if (err) throw err;
+    });
+  });
+
+  //helps print this console.log message in the correct order
+  setTimeout(() => {console.log("inserted 100 malicious hashes");},10);
+
+});
 
 app.use(express.static('public'))
 app.use('/images', express.static('images'))
@@ -13,7 +66,7 @@ app.get('/', function (req, res) {
 
 //GET request to /results and render resultsPage.html file
 app.get('/results', function (req, res) {
-    res.sendFile(__dirname + "/resultsPage.html");
-  })
+  res.sendFile(__dirname + "/resultsPage.html");
+})
 
 app.listen(3000)
