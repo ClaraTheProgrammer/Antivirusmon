5cfb6125940dc6be87cf7347568791e02c4e2686
Alexander Zabriskie <40002805@utcaus.com>
2022-04-03 15:06:25 -0400

    add code for lowdb and creation of database

--- app.js
+++ app.js
@@ -1,76 +1,89 @@
-const express = require('express')
+import { Low, JSONFile } from 'lowdb'
+import express from 'express'
+import fetch from 'node-fetch';
+import fs from 'fs'
+import readline from 'readline'
+import path from 'path';
+
+const __dirname = path.resolve();
+
+const options = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'x-apikey': 'd939e12d943da23cbcbad42125c22fa6dd200bcd713bdcaf1d8c66f1390b2ec5' //'5d7310d34217a642c22cf788592e1e14b552485968825a9db8e47a213d01be48'
+    }
+  };
+
+function VT_Api(hash) {
+    return fetch(`https://www.virustotal.com/api/v3/files/${hash}`, options)
+        .then(response => response.json())
+        .then(data => data.data.attributes)
+        .then(attributes => {
+            return {
+                crowdsourced_ids_stats: attributes.crowdsourced_ids_stats,
+                last_analysis_stats: attributes.last_analysis_stats,
+                md5: attributes.md5,
+                names: attributes.names,
+                reputation: attributes.reputation,
+                sha1: attributes.sha1,
+                sha256: attributes.sha256,
+                sigma_analysis_stats: attributes.sigma_analysis_stats,
+                size: attributes.size,
+                type_description: attributes.type_description,
+                type_extension: attributes.type_extension,
+                type_tag: attributes.type_tag
+            }
+        })
+        .then(hash_info => {
+            db.data[hash] = hash_info
+            db.write()
+        })
+        .catch(error => console.log(error))
+}
+
+const adapter = new JSONFile('db.json')
+const db = new Low(adapter)
+
+await db.read()
+if (db.data == null) {
+    db.data = { }
+    var readLine = readline.createInterface({
+        input: fs.createReadStream('maliciousHashes-dummy.txt')
+    })
+    readLine.on('line', (hash) => VT_Api(hash))
+}
+
 const app = express()
-var mysql = require('mysql');
-const readline = require('readline');
-const fs = require('fs');
-const { resolve } = require('path');
-
-/*
-//database server connection information
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root"
-});
-
-//connect to database server
-con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-
-  //create database within database server
-  con.query("CREATE DATABASE IF NOT EXISTS maliciousHashDB", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
-
-  //connect to database 'mydb'
-  con.query("USE maliciousHashDB", function (err, result) {
-    if (err) throw err;
-    console.log("connected to 'maliciousHashDB' database");
-  });
-
-  //create table
-  var createTableQuery = "CREATE TABLE IF NOT EXISTS maliciousHash (hashID VARCHAR(16000))";
-  con.query(createTableQuery, function (err, result) {
-    if (err) throw err;
-    console.log("created table 'maliciousHash'");
-  });
-
-  //set up read file interface
-  var readLine = require('readline').createInterface({
-    input: require('fs').createReadStream('maliciousHashes.txt')
-  });
-
-  //read maliciousHashes.txt file line by line and insert hash into database
-  readLine.on('line', function (line) {
-    var insertQuery = `INSERT INTO maliciousHash (hashID) VALUES ('${line}')`;
-    con.query(insertQuery, function (err, result) {
-      if (err) throw err;
-    });
-  });
-
-  //helps print this console.log message in the correct order
-  setTimeout(() => { console.log("inserted 100 malicious hashes"); }, 10);
-
-
-});
-*/
-
 app.use(express.static('public'))
 app.use('/images', express.static('images'))
 app.use('/static', express.static('static'))
+app.use('/js', express.static('js'))
 
 //GET request to root of web page and render homePage.html file
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/homePage.html");
 })
 
-
 //GET request to /results and render resultsPage.html file
 app.get('/results', function (req, res) {
   res.sendFile(__dirname + "/resultsPage.html");
 })
 
+//GET request to /seach/:hash and return object representing file hash
+app.get('/search/:hash', async (req, res) => {
+  var hashInfo = db.data[req.params.hash]
+  if (hashInfo == undefined) {
+      await VT_Api(req.params.hash)
+      hashInfo = db.data[req.params.hash]
+  }
+  res.send(hashInfo)
+})
+
+//GET request to /random and return object representing random file hash
+app.get('/random', function (req, res) {
+  const keys = Object.keys(db.data)
+  res.send(db.data[keys[Math.floor(Math.random() * keys.length)]])
+})
 
 app.listen(3000);
